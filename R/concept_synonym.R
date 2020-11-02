1e29c14afbe17dbfa3b864f531b767ca7f2a0c52
David Mayer <dmayer2@gmail.com>
2020-11-01 22:15:50 -0700

    documentation update - additional formatting - add concept synonym and condition era

---
+++ R/concept_synonym.R
@@ -0,0 +1,18 @@
+#' concept_synonym
+#'
+#' @description
+#' ## Table Description
+#'
+#' The CONCEPT_SYNONYM table is used to store alternate names and descriptions for Concepts.
+#'
+#' @docType data
+#'
+#' @format A data frame with XX rows and XX variables:
+#' \describe{
+#'   \item{concept_id }{}
+#'   \item{concept_synonym_name}{}
+#'   \item{language_concept_id}{}
+#'   ...
+#' }
+#' @source \url{https://ohdsi.github.io/CommonDataModel/cdm531.html#concept_synonym}
+"concept_synonym"
