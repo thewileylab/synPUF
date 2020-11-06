9816d5c6626156e0cdcdba88a14e807800df0a46
David Mayer <david.mayer@cuanschutz.edu>
2020-11-06 08:56:12 -0700

    minor documentation formatting tweaks

--- R/vocabulary.R
+++ R/vocabulary.R
@@ -3,7 +3,7 @@
 #' @description
 #' ## Table Description
 #'
-#'The VOCABULARY table includes a list of the Vocabularies collected from various sources or created de novo by the OMOP community. This reference table is populated with a single record for each Vocabulary source and includes a descriptive name and other associated attributes for the Vocabulary.
+#' The VOCABULARY table includes a list of the Vocabularies collected from various sources or created de novo by the OMOP community. This reference table is populated with a single record for each Vocabulary source and includes a descriptive name and other associated attributes for the Vocabulary.
 #'
 #' @docType data
 #'
