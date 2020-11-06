49d915a8bb821e4b0de442aaa00d0226adb2babc
David Mayer <david.mayer@cuanschutz.edu>
2020-11-06 09:08:11 -0700

    update description and add package documentation

---
+++ R/synPUF.R
@@ -0,0 +1,10 @@
+#' synPUF: 50 Person Subset of DE-SynPUF
+#'
+#' This package contains a 50 person subset of the CMS 2008-2010 Data Entrepreneurs’
+#' Synthetic Public Use File (DE-SynPUF) from OHDSI.
+#'
+#'
+#' @docType package
+#' @name synPUF
+NULL
+#> NULL
